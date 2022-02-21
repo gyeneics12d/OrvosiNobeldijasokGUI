3d6c8a5adbf6a928abc8a880446d546bb45609eb
gyeneics12d <gyeneics.12d@acsjszki.hu>
2022-02-21 13:47:49 +0100

    kész az egész

--- Nobel-díj/Form1.cs
+++ Nobel-díj/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Nobel_díj
 {
@@ -19,10 +20,48 @@ namespace Nobel_díj
 
         private void btnMentes_Click(object sender, EventArgs e)
         {
+            int num;
+            bool isNum = int.TryParse(textBox1.Text.Trim(), out num);
             if (textBox1.Text.Trim() == string.Empty)
             {
-                MessageBox.Show("Üres az első mező");
-            } 
+                MessageBox.Show("Nem lehet üres egyik mező sem!");
+            }
+            else if (isNum==false)
+            {
+                MessageBox.Show("Hiba, az évszám nem megfelelő!");
+            }
+            else if (int.Parse(textBox1.Text) < 1989)
+            {
+                MessageBox.Show("Hiba, az évszám nem megfelelő!");
+            }
+            else if (textBox2.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Nem lehet üres egyik mező sem!");
+            }
+            else if (textBox3.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Nem lehet üres egyik mező sem!");
+            }
+            else if (textBox4.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("Nem lehet üres egyik mező sem!");
+            }
+            else
+            {
+                try
+                {
+                    StreamWriter sw = new StreamWriter("uj_dijazott.txt");
+                    sw.WriteLine($"Év;Név;SzületésHalálozás;Országkód" +
+                        $"\n{textBox1.Text};{textBox2.Text};{textBox3.Text};{textBox4.Text}");
+
+                    MessageBox.Show("Sikeres mentés!");
+                    sw.Close();
+                }
+                catch (Exception error)
+                {
+                    MessageBox.Show($"Hiba történt a mentés során. \n {error}");
+                }
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
